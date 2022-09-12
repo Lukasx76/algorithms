d5ca80bf187020beb3184a72aa0303b22f8bafbb
Lucas Gomes <lucas.f.gomes2002@gmail.com>
2022-09-12 20:48:02 -0300

    multiplication table

---
+++ src/CodeWarsChallenge.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class CodeWarsChallenge {
+
+    public static void main(String[] args){
+
+    }
+
+    public static int sum(int[] numbers)
+    {
+        Arrays.stream(numbers).sum();
+        return 0;
+    }
+}
